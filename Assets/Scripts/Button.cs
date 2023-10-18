c242eaf5b7f7e3ca4769b2210791d08b20355cd2
Thibaut <thibaut.berg@hotmail.com>
2023-10-18 21:42:35 +0200

    POC with 4 buttons

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,58 +1,18 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.XR.WSA.Input;
+﻿using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    #region Fields
+    
     public Color defaultColor;
     public Color activeColor;
     
-    private GestureRecognizer m_GestureRecognizer;
     private bool m_IsActive;
 
-    private void Awake()
-    {
-        m_GestureRecognizer = new GestureRecognizer();
-    }
+    #endregion
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        SetColor(defaultColor);
-        
-        m_GestureRecognizer.Tapped += OnTapped;
-        m_GestureRecognizer.StartCapturingGestures();
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        
-    }
-
-    private void OnDestroy()
-    {
-        m_GestureRecognizer.StopCapturingGestures();
-        m_GestureRecognizer.Tapped -= OnTapped;
-        m_GestureRecognizer.Dispose();
-    }
-
-    private void OnTapped(TappedEventArgs tappedEventArgs)
-    {
-        Console.WriteLine($"Tap on {name}");
-        Console.WriteLine($"TapCount = {tappedEventArgs.tapCount}");
-        Console.WriteLine($"Source = {tappedEventArgs.source}");
-        Console.WriteLine($"SourcePose = {tappedEventArgs.sourcePose}");
-        Console.WriteLine($"HeadPose = {tappedEventArgs.headPose}");
-
-        IsActive = false;
-    }
-
-    private void SetColor(Color color)
-    {
-        GetComponent<Renderer>().material.color = color;
-    }
-
+    #region Properties
+    
     public bool IsActive
     {
         get => m_IsActive;
@@ -62,17 +22,33 @@ public class Button : MonoBehaviour
             {
                 m_IsActive = value;
                 
-                if (m_IsActive)
-                {
-                    SetColor(activeColor);
-                    m_GestureRecognizer.StartCapturingGestures();
-                }
-                else
-                {
-                    SetColor(defaultColor);
-                    m_GestureRecognizer.StopCapturingGestures();
-                }
+                Debug.Log("Set IsActive : " + value + " " + name);
+                
+                UpdateColor();
             }
         }
     }
+    
+    #endregion
+
+    #region Methods
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        SetColor(m_IsActive ? activeColor : defaultColor);
+    }
+
+    private void SetColor(Color color)
+    {
+        GetComponent<Renderer>().material.color = color;
+        Debug.Log("Set color of : " + name + " - " + color);
+    }
+    
+    #endregion
 }
---
+++ Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using Random = System.Random;
+
+public class ButtonsManager : MonoBehaviour
+{
+    #region Fields
+
+    private GestureRecognizer m_GestureRecognizer;
+    private List<Button> m_Buttons;
+    private Random m_Random;
+
+    #endregion
+
+    #region Constructors
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        m_Random = new Random();
+        
+        m_GestureRecognizer = new GestureRecognizer();
+        m_GestureRecognizer.StartCapturingGestures();
+        m_GestureRecognizer.Tapped += OnTapped;
+    }
+
+    private void Start()
+    {
+        m_Buttons = FindObjectsOfType<Button>().ToList();
+        Debug.Log("Buttons count : " + m_Buttons.Count);
+
+        if (m_Buttons.Count <= 1)
+        {
+            throw new Exception("Missing buttons for ButtonsManager (must have at least 2 buttons !)");
+        }
+
+        m_Buttons[m_Random.Next(0, m_Buttons.Count - 1)].IsActive = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (m_GestureRecognizer != null)
+        {
+            m_GestureRecognizer.Tapped -= OnTapped;
+            m_GestureRecognizer.StopCapturingGestures();
+            m_GestureRecognizer.Dispose();
+        }
+    }
+
+    private void OnTapped(TappedEventArgs tappedEventArgs)
+    {
+        Debug.Log("OnTapped");
+        
+        // https://docs.unity3d.com/2018.2/Documentation/Manual/SpatialMappingCollider.html
+        var gazeRay = new Ray(tappedEventArgs.headPose.position, tappedEventArgs.headPose.forward);
+        var hits = Physics.RaycastAll(gazeRay, float.MaxValue);
+
+        foreach (var hit in hits)
+        {
+            var targetObject = hit.collider.gameObject;
+            Debug.Log($"Hit Object **\"**{targetObject}**\"** at position **\"**{hit.point}**\"**");
+            
+            var button = targetObject.GetComponent<Button>();
+            if (button != null && button.IsActive)
+            {
+                button.IsActive = false;
+                
+                var nextButton = GetNextButton(button);
+                nextButton.IsActive = true;
+            }
+        }
+    }
+
+    private Button GetNextButton(Button clickedButton)
+    {
+        var iClickedButton = m_Buttons.IndexOf(clickedButton);
+        var iRandomButton = m_Random.Next(0, m_Buttons.Count);
+
+        while (iRandomButton == iClickedButton)
+        {
+            iRandomButton = m_Random.Next(0, m_Buttons.Count - 1);
+        }
+
+        return m_Buttons[iRandomButton];
+    }
+    
+    #endregion
+}
