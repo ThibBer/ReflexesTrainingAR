476e3680714dc7c07e03d8a547b9545e3371ef40
Maxime De Rycke <maxime.derycke@student.unamur.be>
2023-12-10 09:14:36 +0100

    RT-30: Removed dead code

--- Assets/Scripts/HighScore/Score.cs
+++ Assets/Scripts/HighScore/Score.cs
@@ -7,27 +7,17 @@ public struct Score
     // Using private fields instead of public properties since JsonUtility can't serialize properties
     [SerializeField]
     private int score;
-    //[SerializeField]
-    //private string scoreDate;
     [SerializeField]
     private long timestamp;
 
-    // Needed to order the scores by date for the chart
-    //public DateTime ScoreDateTime { get; }
-
     public Score(int score, long timestamp)
     {
+        // JsonUtility can't serialize DateTime
         this.score = score;
         this.timestamp = timestamp;
-        //ScoreDateTime = scoreDate;
-        // JsonUtility can't serialize DateTime
-        // Format exemple: November 12 2023 - 16:50:30
-        //this.scoreDate = scoreDate.ToString("MMMM dd yyyy - H:mm:ss");
     }
 
     public int GetScore() => score;
 
     public long GetTimestamp() => timestamp;
-
-    //public string GetScoreDate() => scoreDate;
 }
